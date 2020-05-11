26e3f2844105fb15f83f05374a59757e627ff579
sun-1999 <41812200@snnu.edu.cn>
2020-05-11 11:50:41 +0800

    '改了Pipfile'

--- make.py
+++ make.py
@@ -1,7 +1,7 @@
 """产生需要的停用词"""
 from itertools import chain
 
-with open('stopwords/all_stopwords.txt', 'w', encoding='utf-8') as f:
+with open('stopwords/use.txt', 'w', encoding='utf-8') as f:
     with open('stopwords/cn_stopwords.txt', encoding='utf-8') as cn,\
         open('stopwords/hit_stopwords.txt', encoding='utf-8') as hit,\
         open('stopwords/scu_stopwords.txt', encoding='utf-8') as scu,\
