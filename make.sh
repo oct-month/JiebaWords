c50a6b3a7bf534c6ac5074e664b2cb898111a6a0
sun-1999 <41812200@snnu.edu.cn>
2020-05-12 22:25:42 +0800

    '第一次打包成功'

--- make.sh
+++ make.sh
@@ -1,11 +1,13 @@
-# 进入虚环境
+# # 进入虚环境
 .venv/Scripts/activate.bat
-# 安装依赖
-pip install -r requirements.txt
-# -w 不显示终端  -F 打包所有库
-pyinstaller -w app.pyw
-# 提供必要文件
+# # 安装依赖
+# pip install -r requirements.txt
+# # -w 不显示终端  -F 打包所有库
+pyinstaller app.pyw -c
+# # 提供必要文件
+
 cp -r files dist/app/
 cp -r jieba dist/app/
 cp -r pyecharts dist/app/
 cp files/drive/win32/chromedriver.exe dist/app/
+rm -r dist/app/files/drive
