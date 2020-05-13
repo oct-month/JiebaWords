c4a494cbfdffcac25186f35b5bcab01eb8e28104
sun-1999 <41812200@snnu.edu.cn>
2020-05-14 00:06:20 +0800

    '优化mak.sh'

--- make.sh
+++ make.sh
@@ -1,10 +1,12 @@
-# # 进入虚环境
-.venv/Scripts/activate.bat
-# # 安装依赖
-# pip install -r requirements.txt
-# # -w 不显示终端  -F 打包所有库 -c 显示终端
+# 环境准备
+pip3 install pipenv -U
+pipenv install --skip-lock
+pipenv shell
+pip install pypiwin32 -U
+
+# -w 不显示终端  -F 打包所有库 -c 显示终端
 pyinstaller -w -i files/img/title.ico app.pyw
-# # 提供必要文件
+# 提供必要文件
 
 cp -r files dist/app/
 cp -r jieba dist/app/
